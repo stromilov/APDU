7e4b381c3d837ab19e788530a11b41543e0db6db
Nick <stromilovnikolay@gmail.com>
2022-06-07 03:46:41 +0700

    Update hex_gen.py

--- hex_gen.py
+++ hex_gen.py
@@ -1,2 +1,4 @@
 text = input()
-print( *['0x' + text[i:i+2] for i in range(0, len(text), 2)] )
+mas = ['0x' + text[i:i+2] for i in range(0, len(text), 2)]
+print("mas length = ", len(mas))
+print("mas = ", *mas)
